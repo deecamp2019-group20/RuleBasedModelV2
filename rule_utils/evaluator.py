a2ab12994e7eab34b088b6889d7166793af4a8ef
closure-d <314233837@qq.com>
2019-08-12 17:27:47 +0800

    t1

--- rule_utils/evaluator.py
+++ rule_utils/evaluator.py
@@ -16,15 +16,33 @@ for c in range(len(action_space_category)):
         elif c <= 3:  # 1单牌, 2对子, 3三条
             v = char2val[a[0]] - 10  # maxCard - 10
             if c == 2 and v > 0:
-                v *= 1.5  # positive + 50%
+                if a == ['2','2']:
+                    v *= 1.2
+                elif a == ['A','A']:
+                    v *= 1.3
+                else:
+                    v *= 1.4  # positive + 50%
             if c == 3 and v > 0:
-                v *= 2  # positive + 100%
+                if a == ['2','2','2']:
+                    v *= 1.1
+                elif a == ['A','A','A']:
+                    v *= 1.5
+                else:
+                    v *= 1.8  # positive + 100%
         elif c == 4:  # 4炸弹
-            v = 9  # 固定9分
+            if a == ['2','2','2','2']:
+                v = 7
+            else:
+                v = 9  # 固定9分
         elif c <= 6:  # 5三带一, 6三带二
             v = char2val[a[0]] - 10  # maxCard - 10
             if v > 0:
-                v *= 1.5  # 带牌比三条加得少
+                if a[:3] == ['2','2','2']:
+                    v *= 1
+                elif a == ['A','A','A']:
+                    v *= 1.2
+                else:
+                    v *= 1.5  # 带牌比三条加得少
         elif c <= 9:  # 7顺子, 8连对, 9飞机
             v = max(0, (char2val[a[-1]] - 10) / 2)  # max(0, (maxCard - 10) / 2)
         elif c == 10:  # 10飞机带小
@@ -40,8 +58,16 @@ for c in range(len(action_space_category)):
                 if char2val[a[i]] > 10:
                     v += 1.5 * (char2val[a[i]] - 10)  # 带牌为正加上
         elif c == 12:  # 12火箭
-            v = 13
+            v = 12
         elif c <= 14:  # 13四带二只, 14四带二对
             v = char2val[a[0]] - 10  # maxCard - 10
+            if v > 0:
+                if a[:4] == ['2','2','2','2']:
+                    v *= 1
+                elif a[:4] == ['A','A','A','A']:
+                    v *= 1.2
+                else:
+                    v *= 1.5  # 带牌比三条加得少
         assert v is not None
+
         cards_value.append(v)
